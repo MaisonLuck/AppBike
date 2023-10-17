6ac164d1fcae9e5b03298c9ebb1b613711aef1ba
Maison dos Santos Luck <maison45996986@aluno.ba.senac.br>
2023-10-17 10:14:14 -0300

    primeiro commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,188 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView, SafeAreaView} from 'react-native';
+  const clickHandler = () =>{
+    alert('Adicionado ao Carrinho!')
+  }
 
 export default function App() {
   return (
+    <ScrollView>
+    <SafeAreaView style={styles.container}>
+    <StatusBar backgroundColor="#4682B4"/>
+   
+      
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+              {/* HEADER */}
+
+        <View style={styles.header}>
+        
+        
+        <Image
+          source ={require("./assets/dark-bike@2x.png")}
+          resizeMode="center"
+          style={{position:"relative", top: 20, right:45}}
+        />
+        <Image
+          source ={require("./assets/dark-bike@3x.png")}
+          resizeMode="center"
+          style={{position:"relative", top: 20}}
+        />
+        <Image
+          source ={require("./assets/helmet.png")}
+          resizeMode="center"
+          style={{position:"relative", top: 17, left:45}}
+          
+        />
+       
+        </View>
+
+                {/* MAIN */}
+
+      <View style={styles.boxProdutos}>
+        <Image
+          source ={require("./assets/bike.png")}
+          style={styles.produtos}
+        />
+        
+        <Text style={styles.text}>Produto 01</Text>
+      </View>
+        <TouchableOpacity onPress={clickHandler}
+        activeOpacity={0.7}
+        style={styles.btAdicionar}>
+        <Text style={styles.textButton}>Adicionar</Text>
+        </TouchableOpacity>
+
+      <View style={styles.boxProdutos}>
+      <Image
+      
+          source ={require("./assets/bike@2x.png")}
+          style={styles.produtos}
+        />
+        <Text style={styles.text}>Produto 02</Text>
+      </View>
+      <TouchableOpacity onPress={clickHandler}
+        activeOpacity={0.7}
+        style={styles.btAdicionar}>
+        <Text style={styles.textButton}>Adicionar</Text>
+        </TouchableOpacity>
+
+      <View style={styles.boxProdutos}>
+      <Image
+          source ={require("./assets/bike@3x.png")}
+          style={styles.produtos3}
+        />
+        <Text style={styles.text}>Produto 03</Text>
+      </View>
+      <TouchableOpacity onPress={clickHandler}
+        activeOpacity={0.7}
+        style={styles.btAdicionar}>
+        <Text style={styles.textButton}>Adicionar</Text>
+        </TouchableOpacity>
+
+      <View style={styles.boxProdutos}>
+      <Image
+          source ={require("./assets/dark-bike.png")}
+          style={styles.produtos}
+        />
+        <Text style={styles.text}>Produto 04</Text>
+      </View>
+      
+      <TouchableOpacity onPress={clickHandler}
+        activeOpacity={0.7}
+        style={styles.btAdicionar4}>
+       <Text style={styles.textButton}>Adicionar</Text>
+        </TouchableOpacity>
+
     </View>
+
+              {/* FOOTER */}
+
+      <View style={styles.boxFooter}>
+      
+      </View>
+    
+    </SafeAreaView>
+    </ScrollView>
+
   );
+  
 }
+
+
+// ESTILIZAÇÃO
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'gray',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
+  header:{
+    backgroundColor:'#fff',
+    width:"100%",
+    justifyContent:"space-evenly",
+    alignItems:"center",
+    display:'flex',
+    flexDirection:'row'
+  },
+    produtos:{
+    width: 200,
+    height: 200
+    
+  },
+  produtos3:{
+    width: 190,
+    height: 130
+  },
+  text:{
+    textAlign: 'center',
+    fontSize: 20,
+    color: "#010101"
+  },
+  boxProdutos:{
+    display:'flex',
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor: "#fff",
+    width: 250,
+    height: 250,
+    borderRadius: 20,
+    marginBottom: 10,
+    marginTop: 35
+  },
+  btAdicionar:{
+    width:150,
+    height: 35,
+    backgroundColor:"#008000",
+    color: "#008000",
+    textAlign:'center',
+    justifyContent:"center",
+    fontSize: 20,
+    borderRadius:10,
+  },
+  btAdicionar4:{
+    width:150,
+    height: 35,
+    backgroundColor:"#008000",
+    textAlign:'center',
+    justifyContent:"center",
+    fontSize: 20,
+    borderRadius:10,
+    marginBottom:50
+  },
+  textButton:{
+    color: "#fff",
+    textAlign:'center'
+  },
+  boxFooter:{
+    display: 'flex',
+    justifyContent:'center',
+    alignItems: 'center',
+    height:50,
+    width:'100%',
+    backgroundColor: "#FFF"
+  }
 });
